1a7ac8b934acc6aeae9fc08db83f48c7a7e34a51
Edward Amor <edward.amor3@gmail.com>
2021-01-24 11:49:20 -0500

    feat: pivot to using ERC721 preset by openzeppelin

--- backend/contracts/BillOfLading.sol
+++ backend/contracts/BillOfLading.sol
@@ -1,12 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.7.5;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/presets/ERC721PresetMinterPauserAutoId.sol";
 
-contract BillOfLading is Ownable, ERC721 {
-    constructor(string memory name, string memory symbol)
-        public
-        ERC721(name, symbol)
-    {}
+// @title Bill of Lading NFT Contract
+// @author Edward Amor
+// @notice Users should not directly interact with this contract
+// @dev All ERC721 functions are inherited from Openzeppelin ERC721 contract
+contract BillOfLading is ERC721PresetMinterPauserAutoId {
+    constructor(
+        string memory name,
+        string memory symbol,
+        string memory baseURI
+    ) public ERC721PresetMinterPauserAutoId(name, symbol, baseURI) {}
 }
