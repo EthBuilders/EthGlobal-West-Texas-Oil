9fe5470cf176e55ec85f90a91b80683bf2515ee8
Edward Amor <edward.amor3@gmail.com>
2021-01-23 21:29:06 -0500

    feat: add test verifying ownership of BOL contract
    Also refactor tests adding a before hook to deploy the contract.

--- backend/test/test_ownership.js
+++ backend/test/test_ownership.js
@@ -1,9 +1,19 @@
 const BillOfLading = artifacts.require("BillOfLading");
 
 contract("BillOfLading", (accounts) => {
+  var instance;
+
+  before("deploy contract", async () => {
+    instance = await BillOfLading.deployed({ from: accounts[0] });
+  });
+
   it("should be owned by the deployer", async () => {
-    let instance = await BillOfLading.deployed({ from: accounts[0] });
     let instance_owner = await instance.owner();
     assert.equal(instance_owner, accounts[0]);
+  });
+
+  it("should not be owned by another account", async () => {
+    let instance_owner = await instance.owner();
+    assert.notEqual(instance_owner, accounts[1]);
   });
 });
