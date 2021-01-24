0e0befb80ef868ff4a8c3046b33fea148140dda1
Edward Amor <edward.amor3@gmail.com>
2021-01-23 22:05:12 -0500

    feat: add ERC165 inheritance to BOL

--- backend/contracts/BillOfLading.sol
+++ backend/contracts/BillOfLading.sol
@@ -2,7 +2,8 @@
 pragma solidity ^0.7.5;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/introspection/ERC165.sol";
 
-contract BillOfLading is Ownable {
+contract BillOfLading is Ownable, ERC165 {
     constructor() public {}
 }
