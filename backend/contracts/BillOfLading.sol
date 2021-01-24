a4f1c113719d27d6b72291e50e40e4ad23182269
Edward Amor <edward.amor3@gmail.com>
2021-01-24 02:38:18 -0500

    feat: add ERC721 inheritance to BOL

--- backend/contracts/BillOfLading.sol
+++ backend/contracts/BillOfLading.sol
@@ -2,8 +2,11 @@
 pragma solidity ^0.7.5;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/introspection/ERC165.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract BillOfLading is Ownable, ERC165 {
-    constructor() public {}
+contract BillOfLading is Ownable, ERC721 {
+    constructor(string memory name, string memory symbol)
+        public
+        ERC721(name, symbol)
+    {}
 }
