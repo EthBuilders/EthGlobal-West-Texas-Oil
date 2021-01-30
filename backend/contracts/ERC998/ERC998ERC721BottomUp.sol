ed35aa8ed0cbf89f899fe20910cf78e19e2f5797
Edward Amor <edward.amor3@gmail.com>
2021-01-30 01:13:56 -0500

    feat: add rootOwnerOf func from composables repo
    Taken directly from https://github.com/mattlockyer/composables-998/blob/master/contracts/ComposableBottomUp.sol

--- backend/contracts/ERC998/ERC998ERC721BottomUp.sol
+++ backend/contracts/ERC998/ERC998ERC721BottomUp.sol
@@ -22,6 +22,156 @@ contract ERC998ERC721BottomUp is IERC998ERC721BottomUp, ERC721 {
     // tokenId => token owner
     mapping(uint256 => TokenOwner) tokenIdToTokenOwner;
 
+    // Use Cases handled:
+    // Case 1: Token owner is this contract and no parent tokenId.
+    // Case 2: Token owner is this contract and token
+    // Case 3: Token owner is top-down composable
+    // Case 4: Token owner is an unknown contract
+    // Case 5: Token owner is a user
+    // Case 6: Token owner is a bottom-up composable
+    // Case 7: Token owner is ERC721 token owned by top-down token
+    // Case 8: Token owner is ERC721 token owned by unknown contract
+    // Case 9: Token owner is ERC721 token owned by user
+    /// @notice Get the root owner of tokenId.
+    /// @param _tokenId The token to query for a root owner address
+    /// @return rootOwner The root owner at the top of tree of tokens and ERC998 magic value.
+    function rootOwnerOf(uint256 _tokenId)
+        public
+        view
+        returns (bytes32 rootOwner)
+    {
+        address rootOwnerAddress = tokenIdToTokenOwner[_tokenId].tokenOwner;
+        require(rootOwnerAddress != address(0));
+        uint256 parentTokenId = tokenIdToTokenOwner[_tokenId].parentTokenId;
+        bool isParent = parentTokenId > 0;
+        parentTokenId--;
+        bytes memory calldata;
+        bool callSuccess;
+
+        if ((rootOwnerAddress == address(this))) {
+            do {
+                if (isParent == false) {
+                    // Case 1: Token owner is this contract and no token.
+                    // This case should not happen.
+                    return
+                        (ERC998_MAGIC_VALUE << 224) | bytes32(rootOwnerAddress);
+                } else {
+                    // Case 2: Token owner is this contract and token
+                    (rootOwnerAddress, parentTokenId, isParent) = _tokenOwnerOf(
+                        parentTokenId
+                    );
+                }
+            } while (rootOwnerAddress == address(this));
+            _tokenId = parentTokenId;
+        }
+
+        if (isParent == false) {
+            // success if this token is owned by a top-down token
+            // 0xed81cdda == rootOwnerOfChild(address, uint256)
+            calldata = abi.encodeWithSelector(
+                0xed81cdda,
+                address(this),
+                _tokenId
+            );
+            assembly {
+                callSuccess := staticcall(
+                    gas,
+                    rootOwnerAddress,
+                    add(calldata, 0x20),
+                    mload(calldata),
+                    calldata,
+                    0x20
+                )
+                if callSuccess {
+                    rootOwner := mload(calldata)
+                }
+            }
+            if (callSuccess == true && rootOwner >> 224 == ERC998_MAGIC_VALUE) {
+                // Case 3: Token owner is top-down composable
+                return rootOwner;
+            } else {
+                // Case 4: Token owner is an unknown contract
+                // Or
+                // Case 5: Token owner is a user
+                return (ERC998_MAGIC_VALUE << 224) | bytes32(rootOwnerAddress);
+            }
+        } else {
+            // 0x43a61a8e == rootOwnerOf(uint256)
+            calldata = abi.encodeWithSelector(0x43a61a8e, parentTokenId);
+            assembly {
+                callSuccess := staticcall(
+                    gas,
+                    rootOwnerAddress,
+                    add(calldata, 0x20),
+                    mload(calldata),
+                    calldata,
+                    0x20
+                )
+                if callSuccess {
+                    rootOwner := mload(calldata)
+                }
+            }
+            if (callSuccess == true && rootOwner >> 224 == ERC998_MAGIC_VALUE) {
+                // Case 6: Token owner is a bottom-up composable
+                // Or
+                // Case 2: Token owner is top-down composable
+                return rootOwner;
+            } else {
+                // token owner is ERC721
+                address childContract = rootOwnerAddress;
+                //0x6352211e == "ownerOf(uint256)"
+                calldata = abi.encodeWithSelector(0x6352211e, parentTokenId);
+                assembly {
+                    callSuccess := staticcall(
+                        gas,
+                        rootOwnerAddress,
+                        add(calldata, 0x20),
+                        mload(calldata),
+                        calldata,
+                        0x20
+                    )
+                    if callSuccess {
+                        rootOwnerAddress := mload(calldata)
+                    }
+                }
+                require(callSuccess, "Call to ownerOf failed");
+
+                // 0xed81cdda == rootOwnerOfChild(address,uint256)
+                calldata = abi.encodeWithSelector(
+                    0xed81cdda,
+                    childContract,
+                    parentTokenId
+                );
+                assembly {
+                    callSuccess := staticcall(
+                        gas,
+                        rootOwnerAddress,
+                        add(calldata, 0x20),
+                        mload(calldata),
+                        calldata,
+                        0x20
+                    )
+                    if callSuccess {
+                        rootOwner := mload(calldata)
+                    }
+                }
+                if (
+                    callSuccess == true &&
+                    rootOwner >> 224 == ERC998_MAGIC_VALUE
+                ) {
+                    // Case 7: Token owner is ERC721 token owned by top-down token
+                    return rootOwner;
+                } else {
+                    // Case 8: Token owner is ERC721 token owned by unknown contract
+                    // Or
+                    // Case 9: Token owner is ERC721 token owned by user
+                    return
+                        (ERC998_MAGIC_VALUE << 224) | bytes32(rootOwnerAddress);
+                }
+            }
+        }
+    }
+
     function _tokenOwnerOf(uint256 _tokenId)
         internal
         view
