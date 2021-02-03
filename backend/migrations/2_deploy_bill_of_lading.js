e8e26a179edbd5d4d33b79d9b292a7b217ed8e82
Edward Amor <edward.amor3@gmail.com>
2021-02-02 23:50:14 -0500

    feat: update migration script and truffle config

--- backend/migrations/2_deploy_bill_of_lading.js
+++ backend/migrations/2_deploy_bill_of_lading.js
@@ -1,5 +1,9 @@
 const BillOfLading = artifacts.require("BillOfLading");
+const DBol = artifacts.require("DBol");
 
-module.exports = function (deployer) {
-  deployer.deploy(BillOfLading, "Bill Of Lading", "BOL", "https://bol.io/");
+module.exports = async function (deployer, accounts, web3) {
+  //ganache-cli --gasLimit 0xfffffffffff -g 0x01 --allowUnlimitedContractSize      
+  deployer.deploy(DBol, "", "", "").then(() => {
+    return deployer.deploy(BillOfLading, "", "", "", DBol.address);
+  })
 };
