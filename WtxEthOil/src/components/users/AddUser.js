5531ec8855d95b1e6bfee6083a215cdbde5427e2
Edward Amor <edward.amor3@gmail.com>
2021-02-06 16:18:44 -0500

    fix: add contract address input

--- WtxEthOil/src/components/users/AddUser.js
+++ WtxEthOil/src/components/users/AddUser.js
@@ -5,6 +5,7 @@ import InputField from "../layout/Input";
 
 const AddUser = () => {
   let history = useHistory();
+  let [bol, setBol] = useState("0x0000000000000000000000000000000000000000");
   const [user, setUser] = useState({
     driver: '',
     serialNumber: '',
@@ -31,21 +32,28 @@ const AddUser = () => {
   };
 
   return (
-    <div className='container'>
-      <div className='w-75 mx-auto shadow p-5'>
-        <h2 className='text-center mb-4'>Add A BOL</h2>
-        <form onSubmit={(e) => onSubmit(e)}>
-          {Object.keys(user).map((key, index) => (
-            <div className='form-group' key={index}>
-              <InputField fieldName={key} fieldValue={user[key]} setField={onInputChange} type="text" placeholder={key} />
-            </div>
-          ))}
-          <button className='btn btn-primary btn-block'>Add BOL</button>
-        </form>
+    <div>
+      <div className='container'>
+        <div className='w-75 mx-auto shadow p-5'>
+          <h2 className='text-center mb-4'>BOL Contract Address</h2>
+          <InputField fieldName="" fieldValue={bol} setField={setBol} type="text" placeholder="Enter an address" />
+        </div>
+      </div>
+      <div className='container'>
+        <div className='w-75 mx-auto shadow p-5'>
+          <h2 className='text-center mb-4'>Add A BOL</h2>
+          <form onSubmit={(e) => onSubmit(e)}>
+            {Object.keys(user).map((key, index) => (
+              <div className='form-group' key={index}>
+                <InputField fieldName={key} fieldValue={user[key]} setField={onInputChange} type="text" placeholder={key} />
+              </div>
+            ))}
+            <button className='btn btn-primary btn-block'>Add BOL</button>
+          </form>
+        </div>
       </div>
     </div>
   );
-
 };
 
 export default AddUser;
